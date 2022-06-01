4a77bf05b9f3eae6a11b504356ac472e74e497f8
skalnark <darcio.basiliodasilva@orbee.com>
2022-06-01 12:26:41 -0300

    fix bspwm conditional

--- config/polybar/launch_single.sh
+++ config/polybar/launch_single.sh
@@ -12,5 +12,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
 
-polybar -q main -c "$DIR"/config.ini &
-polybar -q external -c "$DIR"/config.ini &
+polybar -q single -c "$DIR"/config.ini &
